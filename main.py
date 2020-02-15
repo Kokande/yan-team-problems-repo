c933df82126596c28cef2b32c05fbdfb986566d7
Kokande <sdobrovna@gmail.com>
2020-02-16 00:24:33 +0500

    Полностью выполненное задание №1, оболочка готова, можно разделяться

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPixmap
 import sys
 import requests
 from PyQt5 import uic
+import os
 
 
 API = "http://static-maps.yandex.ru/1.x/"
@@ -11,13 +13,26 @@ class Problem(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
+        self.update_map()
         self.initUI()
 
     def initUI(self):
         self.update_b.clicked.connect(self.update_map)
 
     def update_map(self):
-        pass
+        params = {
+            'z': self.z.value(),
+            'll': ','.join([self.longitude.text(), self.lattitude.text()]),
+            'l': 'map'
+        }
+        response = requests.get(API, params=params)
+        if response:
+            with open("map.png", 'wb') as img:
+                img.write(response.content)
+            self.map.setPixmap(QPixmap("map.png"))
+
+    def closeEvent(self, *args, **kwargs):
+        os.remove("map.png")
 
 
 if __name__ == '__main__':
